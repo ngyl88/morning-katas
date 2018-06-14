68907da48b9a3a0af06e76de3c76f1664715086c
ngyl88 <ng_yl88@hotmail.com>
2018-06-14 16:16:35 +0800

    TDD with Derek

--- src/decodeMessage.js
+++ src/decodeMessage.js
@@ -4,8 +4,20 @@ For example: "a" is encoded with "z", "b" with "y", "c" with "x", etc
 Spaces are not encoded/decoded. For example: "gsrh nvhhztv" will be decoded as "this message"
 */
 
-const decodeMessage = (string) => {
 
+const getMappedChar = char => {
+    if(char === ' ') return ' ';
+
+    const alphaSequence = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    const originalIndex = alphaSequence.indexOf(char);
+    return alphaSequence.reverse()[originalIndex];
+}
+
+const decodeMessage = string => {
+  return string
+    .split("")
+    .map(char => getMappedChar(char))
+    .join("");
 };
 
 module.exports = decodeMessage;
--- test/decodeMessage.test.js
+++ test/decodeMessage.test.js
@@ -1,6 +1,15 @@
 const decodeMessage = require("../src/decodeMessage");
 const assertEquals = require("../assert-helper");
 
+assertEquals(decodeMessage('b'), 'y');
+assertEquals(decodeMessage('y'), 'b');
+assertEquals(decodeMessage('yb'), 'by');
+assertEquals(decodeMessage('hi'), 'sr');
+assertEquals(decodeMessage('svl'), 'heo');
+assertEquals(decodeMessage('abcdefghijklmnopqrstuvwxyz'), 'zyxwvutsrqponmlkjihgfedcba');
+assertEquals(decodeMessage('this message'), 'gsrh nvhhztv');
+assertEquals(decodeMessage('r slkv mlylwb wvxlwvh gsrh nvhhztv'), 'i hope nobody decodes this message');
+
 // Test cases: actual --> expected
 // decodeMessage("a") --> "z"
 // decodeMessage("z") --> "a"
